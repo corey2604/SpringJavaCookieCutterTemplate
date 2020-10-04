9d7270354f9aadec096d2b576a8a55ddae39ab2f
corey-duffy <coreyjduffy@hotmail.com>
2020-10-04 15:08:03 +0100

    Adding initial template structure

---
+++ {{cookiecutter.directory_name}}/src/main/java/prj/coreyduffy/{{cookiecutter.directory_name}}/{{cookiecutter.directory_name}}Application.java
@@ -0,0 +1,13 @@
+package prj.coreyduffy.{{cookiecutter.directory_name}};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class {{cookiecutter.directory_name}}Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run({{cookiecutter.directory_name}}Application.class, args);
+	}
+
+}
---
+++ {{cookiecutter.directory_name}}/src/test/java/prj/coreyduffy/{{cookiecutter.directory_name}}/{{cookiecutter.directory_name}}ApplicationTests.java
@@ -0,0 +1,13 @@
+package prj.coreyduffy.{{cookiecutter.directory_name}};
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class {{cookiecutter.directory_name}}ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
